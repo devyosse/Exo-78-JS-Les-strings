313321af5c6b31eab6884d5f1034bde68700edb4
devyosse <narutoshippuden884@gmail.com>
2021-11-24 16:39:26 +0100

    exo 78

--- exercice1.js
+++ exercice1.js
@@ -1,16 +1,21 @@
 let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
 
 // Completer le code suivant pour afficher la longueur de la variable monTexte
-
-alert(monTexte.);
-
+let longueur = monTexte.length;
+alert(monTexte);
+console.log(monTexte)
 
 //Ajouter du code ci dessous pour réaliser la suite de l'exercice
 
+let position = monTexte.indexOf("i");
+document.getElementById('position').innerHTML = position;
 
+let lettre = monTexte.toUpperCase();
+console.log(lettre);
 
+document.getElementById('chaineMaj').innerHTML = lettre;
 
-
-
-
+for(let i= 0; i<monTexte.length; i++){
+    alert(monTexte[i] === 'a');
+}
 
